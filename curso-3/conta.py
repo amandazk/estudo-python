84eb7de0757ba40855767c541e1b8f0f3b509dae
Amanda Zacharuk <zacharukaz@gmail.com>
2021-06-25 13:26:58 -0300

    Cap 3

--- curso-3/conta.py
+++ curso-3/conta.py
@@ -6,5 +6,20 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
+    def extrato(self):
+        print("O saldo do titular {} é {} reais".format(self.titular, self.saldo))
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
 conta = Conta(123, "Amanda", 1000.0, 3000.0)
-print(conta.titular)
+conta2 = Conta(133, "Carlos", 2500.0, 7000.0)
+
+conta.extrato()
+conta2.extrato()
+
+conta2.deposita(1000.0)
+conta2.extrato()
