eb57199f5efedb42c748ba1d17f280dd4836ae2c
Amanda Zacharuk <zacharukaz@gmail.com>
2021-06-30 14:19:00 -0300

    Cap 4

--- curso-3/conta.py
+++ curso-3/conta.py
@@ -1,7 +1,7 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        self.__numero = numero # __ server para definir o atributo como privado
+        self.__numero = numero # __ serve para definir o atributo como privado
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
@@ -15,6 +15,11 @@ class Conta:
     def saca(self, valor):
         self.__saldo -= valor
 
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+# Testes
 conta = Conta(123, "Amanda", 1000.0, 3000.0)
 conta2 = Conta(133, "Carlos", 2500.0, 7000.0)
 
@@ -22,4 +27,8 @@ conta.extrato()
 conta2.extrato()
 
 conta2.deposita(1000.0)
+conta2.extrato()
+
+conta.transfere(10.0, conta2)
+conta.extrato()
 conta2.extrato()
