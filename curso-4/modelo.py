97e84a9f3639066bbcb0a3a79a65fe2c0264c227
Amanda Zacharuk <zacharukaz@gmail.com>
2021-07-04 15:42:25 -0300

    Cap 3 - Curso 4 - Polimorfismo

--- curso-4/modelo.py
+++ curso-4/modelo.py
@@ -19,22 +19,33 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
+    def __str__(self):
+        return '{} - {} - {} like(s)'.format(self._nome, self.ano, self._likes)
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano) # pega do construtor da classe mãe (Programa)
         self.duracao = duracao
+
+    def __str__(self):
+        return '{} - {} - {} min - {} like(s)'.format(self._nome, self.ano, self.duracao, self._likes)
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return '{} - {} - {} temporada(s) - {} like(s)'.format(self._nome, self.ano, self.temporadas, self._likes)
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print('{} - {} : {}'.format(vingadores.nome, vingadores.duracao, vingadores.likes))
 
 mindhunter = Serie('Mindunter', 2019, 2)
 mindhunter.dar_like()
 mindhunter.dar_like()
 
-print('{} - {} : {}'.format(mindhunter.nome, mindhunter.temporadas, mindhunter.likes))
+filmes_e_series = [vingadores, mindhunter]
+for programa in filmes_e_series:
+    print(programa)
