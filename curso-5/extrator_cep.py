4e9a445573eed5c8cfbf5efbd14c99e7bf08aec0
Amanda Zacharuk <zacharukaz@gmail.com>
2021-07-16 13:15:16 -0300

    Quantificadores e intervalos

--- curso-5/extrator_cep.py
+++ curso-5/extrator_cep.py
@@ -3,7 +3,7 @@ import re # Regular Expression
 endereco = "Rua Buena Vista, número 200, Boa Vista, Joinville, SC, 23440-120"
 # CEP = 5 dígitos + hífen(opcional) + 3 dígitos
 
-padrao =  re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao =  re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 busca = padrao.search(endereco)
 
 if busca:
