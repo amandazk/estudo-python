fad5b6e827ac628a64911317c94b7bf236f3cf7a
Amanda Zacharuk <zacharukaz@gmail.com>
2021-08-17 17:23:07 -0300

    Utilizando um pacote

--- curso-8/Cpf.py
+++ curso-8/Cpf.py
@@ -1,5 +1,7 @@
-class Cpf:
+from validate_docbr import CPF
 
+class Cpf:
+   
     def __init__(self, documento):
         documento = str(documento)
         if self.cpf_eh_valido(documento):
@@ -10,23 +12,13 @@ class Cpf:
     def __str__(self):
         return self.formata_cpf()
 
-    def cpf_eh_valido(self, documento):
-        if len(documento) == 11:
-            return True
+    def cpf_eh_valido(self, cpf):
+        if len(cpf) == 11:
+            validador = CPF()
+            return validador.validate(cpf)
         else:
-            return False
+            raise ValueError("Quantidade de dígitos inválida")
 
     def formata_cpf(self):
-        fatia_um = self.cpf[:3]
-        fatia_dois = self.cpf[3:6]
-        fatia_tres = self.cpf[6:9]
-        fatia_quatro = self.cpf[9:]
-
-        return(
-            "{}.{}.{}-{}".format(
-            fatia_um, 
-            fatia_dois, 
-            fatia_tres, 
-            fatia_quatro
-            )
-        )
+       mascara = CPF()
+       return mascara.mask(self.cpf)
--- curso-8/main.py
+++ curso-8/main.py
@@ -1,7 +1,4 @@
-from validate_docbr import CPF
 from Cpf import Cpf
 
-cpf = CPF()
-
-print(cpf.validate("19993663018"))
-print(cpf.validate("11111111111"))
+cpf_um = Cpf("19993663018")
+print(cpf_um)
