3c10c4dac7d48bbc064af566e06b0920a36af468
Amanda Zacharuk <zacharukaz@gmail.com>
2021-08-19 15:22:27 -0300

    Validando CNPJ

---
+++ curso-8/cpf_cnpj.py
@@ -0,0 +1,42 @@
+from validate_docbr import CPF, CNPJ
+import validate_docbr
+
+class CpfCnpj:
+   
+    def __init__(self, documento, tipo_documento):
+        self.tipo_documento = tipo_documento
+        documento = str(documento)
+
+        if tipo_documento == "cpf":
+            if self.cpf_eh_valido(documento):
+                self.cpf = documento
+            else:
+                raise ValueError("CPF Inválido")
+        elif tipo_documento == "cnpj":
+            if self.cnpj_eh_valido(documento):
+                self.cnpj = documento
+            else:
+                raise ValueError("CNPJ Inválido")
+        else:
+            raise ValueError("Tipo de documento inválido")
+
+    def __str__(self):
+        return self.formata_cpf()
+
+    def cpf_eh_valido(self, cpf):
+        if len(cpf) == 11:
+            validador = CPF()
+            return validador.validate(cpf)
+        else:
+            raise ValueError("Quantidade de dígitos inválida")
+
+    def formata_cpf(self):
+       mascara = CPF()
+       return mascara.mask(self.cpf)
+
+    def cnpj_eh_valido(self, cnpj):
+        if len(cnpj) == 14:
+            validador = CNPJ()
+            return validador.validate(cnpj) 
+        else:
+            raise ValueError("Quantidade de dígitos inválida")
--- curso-8/main.py
+++ curso-8/main.py
@@ -1,4 +1,7 @@
-from Cpf import Cpf
+from cpf_cnpj import CpfCnpj
 
-cpf_um = Cpf("19993663018")
-print(cpf_um)
+# cpf_um = Cpf("19993663018")
+# print(cpf_um)
+
+cnpj = "03264059000185"
+documento = CpfCnpj(cnpj, "cnpj")
