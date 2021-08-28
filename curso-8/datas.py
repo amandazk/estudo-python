4ec4c085c33516852dacef05fa766289ef1ac020
Amanda Zacharuk <zacharukaz@gmail.com>
2021-08-28 16:31:18 -0300

    Introducao APIs e validação de CEP

--- curso-8/datas.py
+++ curso-8/datas.py
@@ -1,10 +1,5 @@
-from datetime import date, datetime, timedelta
-from datas_br import DatasBR
+from acesso_cep import BuscaEndereco
 
-'''hoje = datetime.today()
-amanha = datetime.today() + timedelta(days=1, hours=12)
-
-print(amanha-hoje)'''
-
-hoje = DatasBR()
-print(hoje.tempo_cadastro())
+cep = 45055405
+objeto_cep = BuscaEndereco(cep)
+print(objeto_cep)
