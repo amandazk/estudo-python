0954ffce1e324e8f107cf302695472a1589926fb
Amanda Zacharuk <zacharukaz@gmail.com>
2021-09-06 15:16:07 -0300

    Implementando o avaliador de leilões

--- curso-9/leilao/principal.py
+++ curso-9/leilao/principal.py
@@ -1,4 +1,4 @@
-from dominio import Usuario, Lance, Leilao
+from dominio import Usuario, Lance, Leilao, Avaliador
 
 amanda = Usuario("Amanda")
 joao = Usuario("Joao")
@@ -8,8 +8,13 @@ lance_joao = Lance(joao, 75)
 
 leilao = Leilao("Computador")
 
-leilao.lances.append(lance_amanda)
 leilao.lances.append(lance_joao)
+leilao.lances.append(lance_amanda)
 
 for lance in leilao.lances:
     print("O usuário {} deu um lance de {} reais".format(lance.usuario.nome, lance.valor))
+
+avaliador = Avaliador()
+avaliador.avalia(leilao)
+print("O menor lance foi de {} reais e o maior lance foi de {} reais"
+      .format(avaliador.menor_lance, avaliador.maior_lance))
