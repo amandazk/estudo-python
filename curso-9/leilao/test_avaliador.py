e069a57fca9efad827657d73e8f8d4e7c20ef1ed
Amanda Zacharuk <zacharukaz@gmail.com>
2021-09-06 17:46:22 -0300

    Renomeando os testes

--- curso-9/leilao/test_avaliador.py
+++ curso-9/leilao/test_avaliador.py
@@ -3,7 +3,7 @@ from dominio import Usuario, Lance, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         amanda = Usuario("Amanda")
         joao = Usuario("Joao")
 
@@ -24,7 +24,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
         self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
 
-    def test_avalia_2(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
         amanda = Usuario("Amanda")
         joao = Usuario("Joao")
 
