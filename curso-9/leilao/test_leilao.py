fc336c7581fc6ecb1ba12d4a39c4e7fe4735ea73
Amanda Zacharuk <zacharukaz@gmail.com>
2021-09-20 18:25:45 -0300

    Mudanças nos testes e domínio

--- curso-9/leilao/test_leilao.py
+++ curso-9/leilao/test_leilao.py
@@ -22,18 +22,14 @@ class TestLeilao(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
-        joao = Usuario("Joao")
-        lance_joao = Lance(joao, 75)
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
 
-        self.leilao.propoe(self.lance_amanda)
-        self.leilao.propoe(lance_joao)
+        with self.assertRaises(ValueError):
+            joao = Usuario("Joao")
+            lance_joao = Lance(joao, 75)
 
-        menor_valor_esperado = 75
-        maior_valor_esperado = 200
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+            self.leilao.propoe(self.lance_amanda)
+            self.leilao.propoe(lance_joao)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_o_leilao_tiver_um_lance(self):
         self.leilao.propoe(self.lance_amanda)
@@ -44,15 +40,15 @@ class TestLeilao(TestCase):
     def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
         joao = Usuario("Joao")
         ana = Usuario("Ana")
-        lance_joao = Lance(joao, 75)
-        lance_ana = Lance(ana, 900)
+        lance_joao = Lance(joao, 500)
+        lance_ana = Lance(ana, 950)
 
         self.leilao.propoe(self.lance_amanda)
         self.leilao.propoe(lance_joao)
         self.leilao.propoe(lance_ana)
 
-        menor_valor_esperado = 75
-        maior_valor_esperado = 900
+        menor_valor_esperado = 200
+        maior_valor_esperado = 950
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
